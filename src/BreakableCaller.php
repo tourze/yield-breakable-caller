f2ea819a1e8e44f7473034c6a8606c4252b4cffd
github-ci <you@example.com>
2025-04-27 04:00:04 +0000

    feat(yield-breakable-caller): 添加新包及其相关配置和文档

---
+++ src/BreakableCaller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tourze\YieldBreakableCaller;
+
+/**
+ * 一个基于yield的invoker，支持业务中断
+ */
+class BreakableCaller
+{
+    public function invoke(callable $callback, callable $shouldNext): void
+    {
+        // 调用回调函数执行任务
+        /** @var \Generator $generator */
+        $generator = $callback();
+
+        // 模拟循环迭代任务
+        while ($generator->valid()) {
+            // 继续执行生成器函数的部分代码
+            $generator->next();
+
+            // 根据逻辑判断是否继续任务
+            if (!$shouldNext()) {
+                // $generator->send('break');
+                break;
+            }
+        }
+    }
+}
